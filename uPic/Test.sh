c774ae8150d429e32784be8f740703649ad5290e
s-cloud-bk <54706876+s-cloud-bk@users.noreply.github.com>
2020-08-28 23:06:19 +0800

    ⬆ Uploaded by uPic 👉❤️ Powered by https://github.com/gee1k/uPic ❤️👈

---
+++ uPic/Test.sh
@@ -0,0 +1,7 @@
+cd ../../themes
+yum wget
+wget https://cdn.jsdelivr.net/gh/s-cloud-bk/Upic@master/uPic/Miracle_Deluxe-master.zip
+tar -xzvf Miracle_Deluxe-master.zip
+mv Miracle_Deluxe-master Miracle
+rm -rf Miracle_Deluxe-master
+rm Miracle_Deluxe-master.zip
